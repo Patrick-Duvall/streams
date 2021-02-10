8038f6e7c1b3516a9722a0c896f2253c675b8118
Patrick Duvall <duvallp208@gmail.com>
2021-02-10 15:39:17 -0700

    Add Auth reducer

---
+++ client/src/reducers/authReducer.jsx
@@ -0,0 +1,16 @@
+import { Switch } from "react-router-dom";
+
+const INITIAL_STATE = {
+  isSignedIn: null
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SIGN_IN':
+      return {...state, isSignedIn: true}
+    case 'SIGN_OUT':
+      return {...state, isSignedIn: false}
+    default:
+      return state
+  }
+};
