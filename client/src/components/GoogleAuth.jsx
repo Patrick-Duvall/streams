934178c436e55ed822afc4f5d16cc7a69a10c8ab
Patrick Duvall <duvallp208@gmail.com>
2021-02-10 13:32:26 -0700

    add onAuthChange event listener

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 
 class GoogleAuth extends React.Component {
+  state= { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '112191001931-3ccgjmalqd9h7r42njmi4bttpdpdbabc.apps.googleusercontent.com',
         scope: 'email'
-      })
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange)
+      });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+  }
+
+  renderAuthButton() {
+    if(this.state.isSignedIn === null) {
+      return <div>Sign in status unknown</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>
+    } else {
+      return <div>I am not signed in</div>
+    }
+  }
 
   render() {
-    return <div>Google Auth</div>
+    return <div>{this.renderAuthButton()}</div>
   }
 }
 
