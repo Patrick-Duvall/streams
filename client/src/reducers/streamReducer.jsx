e3404aecbabf4985fa2f54f2172ecb9ccde0be9b
Patrick Duvall <duvallp208@gmail.com>
2021-02-13 15:08:23 -0700

    Refactor Stream Create

--- client/src/reducers/streamReducer.jsx
+++ client/src/reducers/streamReducer.jsx
@@ -8,7 +8,6 @@ import {
 } from '../actions/types'
 
 export default (state = {}, action) => {
-  console.log(action.payload);
   switch (action.type) {
     case FETCH_STREAMS:
       return {...state, ..._.mapKeys(action.payload, 'id')}
