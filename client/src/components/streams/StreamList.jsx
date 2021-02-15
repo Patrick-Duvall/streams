29ceda3baa959e3c93cacd1079a83609c128d2fd
Patrick Duvall <duvallp208@gmail.com>
2021-02-15 09:18:41 -0700

    Add Switch

--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -26,18 +26,18 @@ class StreamList extends React.Component{
 
   renderList() {
     return this.props.streams.map( stream => {
-      return(
-        <div className='item' key={stream.id}>
+      return (
+        <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
-          <i className='large middle aligned icon camera' />
-          <div className='content'>
-            {stream.title}
-            <div className='description'>
-              {stream.description}
-            </div>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            <Link className="header" to={`/streams/${stream.id}`}>
+              {stream.title}
+            </Link>
+            <div className="description">{stream.description}</div>
           </div>
         </div>
-      )
+      );
     })
   }
 
